2d8b379d8e840ec485c73f91df146336c07c5dca
pluszak <luszukpawel@gmail.com>
2018-03-18 14:43:55 +0100

    create project + base clases of state and CSP

---
+++ Assets/CSP.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class CSP : MonoBehaviour
+{
+
+    public Color DomainValues;
+    public List<State> Positions = new List<State>();
+
+
+    void Start()
+    {
+        Positions.Add(new State(0, new List<Vector2>
+        {
+            new Vector2(1f,1f),
+            new Vector2(2f,0f),
+            new Vector2(3f,1f),
+        }));
+
+        Positions.Add(new State(1, new List<Vector2>
+        {
+            new Vector2(0f,2f),
+            new Vector2(2f,0f),
+            new Vector2(3f,1f),
+        }));
+    }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class State
+{
+    public int StateIndex;
+    private List<Vector2> Coordinates;
+
+    
+    public State(int stateIndex,List<Vector2> coordinates)
+    {
+        StateIndex = stateIndex;
+        Coordinates = coordinates;
+    }
+}
